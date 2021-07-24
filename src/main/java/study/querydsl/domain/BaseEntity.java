501d5344e4fe26469a08bf4d8c277b4c1ae4e586
KBY-TECH <soccer_by@naver.com>
2021-07-24 23:37:38 +0900

    feat : MappedSuperclass

---
+++ src/main/java/study/querydsl/domain/BaseEntity.java
@@ -0,0 +1,24 @@
+package study.querydsl.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(value = {AuditingEntityListener.class})
+@Getter
+public abstract class BaseEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
