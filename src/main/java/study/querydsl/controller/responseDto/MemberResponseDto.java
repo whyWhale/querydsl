014363ae00cbd8288dd33728cfb477a5abeb4e31
KBY-TECH <soccer_by@naver.com>
2021-07-24 23:42:17 +0900

    feat : MemberResponseDto Field add(id,createdDate)

--- src/main/java/study/querydsl/controller/responseDto/MemberResponseDto.java
+++ src/main/java/study/querydsl/controller/responseDto/MemberResponseDto.java
@@ -1,18 +1,19 @@
 package study.querydsl.controller.responseDto;
 
 import com.querydsl.core.annotations.QueryProjection;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString
 @Getter
 public class MemberResponseDto {
+    private Long id;
     private String name;
     private int age;
-
+    private String createdDate;
 
     @QueryProjection
     public MemberResponseDto(String name, int age) {
@@ -20,5 +21,16 @@ public class MemberResponseDto {
         this.age = age;
     }
 
+    @Builder
+    public MemberResponseDto(Long id,String name, int age, LocalDateTime createdDate) {
+        this.id=id;
+        this.name = name;
+        this.age = age;
+        this.createdDate = formatDate(createdDate);
+    }
 
+    public String formatDate(LocalDateTime localDateTime)
+    {
+        return localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd-HH:ss"));
+    }
 }
